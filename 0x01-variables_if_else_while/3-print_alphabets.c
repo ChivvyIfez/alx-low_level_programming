e64a7d0173f52bf1ce112bf0b203e8de45182ba0
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-09 16:12:55 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	char b;
 
-	for (b = 'a'; b <= 'z'; c++)
+	for (b = 'a'; b <= 'z'; b++)
 	{
 		putchar(b);
 	}
 
-	for (b = 'A'; b <= 'Z'; c++)
+	for (b = 'A'; b <= 'Z'; b++)
 	{
 		putchar(b);
 	}
