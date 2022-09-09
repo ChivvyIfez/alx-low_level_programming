08451c145b730cc52f9a32d538fea2a5909a1a43
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-09 16:02:46 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Printable alphabets upper and lower
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; c++)
+	{
+		putchar(b);
+	}
+
+	for (b = 'A'; b <= 'Z'; c++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
