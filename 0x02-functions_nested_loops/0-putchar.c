82bc0ba0476029a00774fc5847fd90554a54ccf4
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-13 20:13:26 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- main - Prints a word
+ * main - Prints a word
  *
- *Return 0
- **/
+ * Return 0
+ */
 
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
