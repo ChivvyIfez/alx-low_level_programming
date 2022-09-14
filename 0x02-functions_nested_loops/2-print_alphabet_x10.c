b8deb3726ed8cfd58b9420624aaa0bd9c421b5e4
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-13 20:54:57 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet by x10 followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char abc;
+	abc = 'a';
+	int j = 0;
+
+	while ( j < 10)
+	{
+		while (abc <= 'z')
+		{
+			_putchar(abc);
+			abc++;
+		}
+		_putchar('\n');
+		j++
+	}
+}
