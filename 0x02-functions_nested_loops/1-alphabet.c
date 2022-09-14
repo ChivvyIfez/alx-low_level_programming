50d2fcc4f7424f26c46eacb4ec8f3a55a5d46958
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-13 20:41:22 -0700

    Task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * main - Print alphabet in lowercase followed by a new line
+ *main - print alphabet in lowercase followed by a new line
  */
 
 void print_alphabet(void)
 {
 	char alpha;
+
 	alpha = 'a';
 
 	while (alpha <= 'z')
 	{
 		_putchar(alpha);
-		alpha ++;
+		alpha++;
 
 	}
 	_putchar('\n');
