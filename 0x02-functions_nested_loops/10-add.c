d0562b8c085d4c385dce44e567e43200fde4109e
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-13 22:42:20 -0700

    Task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds two numbers
+ * @a: to add to b
+ * @b: to add to a
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int c;
+	c = a + b;
+	return c;
+}
