7ef45db4fa3c0236f342c17f7d3c3120f284b208
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-13 22:57:48 -0700

    Task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints number from 0 to 98 in order
+ */
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <=98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d, ". i);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
+}
