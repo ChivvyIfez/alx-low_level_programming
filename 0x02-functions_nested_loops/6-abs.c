b3b5d7a566f905983f20fca87770684556a968da
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-13 21:49:38 -0700

    Task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of int
+ * @n: check
+ * Return: The absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
