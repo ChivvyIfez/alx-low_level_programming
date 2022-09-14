cb5de9257d2109334a6ee8bdda6dc123ed7858a3
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-14 15:03:14 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,12 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <=98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d, ", i);
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
