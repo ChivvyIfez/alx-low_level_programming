511cb5eac627d40d5ec0e8779429cafa5b5e1f02
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-14 17:31:06 -0700

    Task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,8 +9,10 @@
 int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
+
 	j = 1;
 	k = 2;
+	
 	printf("%lu", j);
 
 	for (i = 0; i < 91; i++)
@@ -19,15 +21,15 @@ int main(void)
 		k = k + j;
 		j = k - j;
 	}
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
+	j1 = j / 100000000;
+	j2 = j % 100000000;
+	k1 = k / 100000000;
+	k2 = k % 100000000;
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf(", %lu", k2 % 1000000000);
+		printf(", %lu", k1 + (k2 / 100000000));
+		printf(", %lu", k2 % 100000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
 		k2 = k2 + j2;
