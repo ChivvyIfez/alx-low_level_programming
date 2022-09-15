45efd584520906a26b1ab5206cdf9d5ebff27d81
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-15 15:24:11 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main'h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number of time to print _
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 1;
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
