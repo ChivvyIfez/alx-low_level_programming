8270432a361ac86f81bf6a3f21b8ee72925ca0b1
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-15 16:00:40 -0700

    Task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		i = 0;
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			i++;
+			_putchar('\n');
+		}
+	}
+}
