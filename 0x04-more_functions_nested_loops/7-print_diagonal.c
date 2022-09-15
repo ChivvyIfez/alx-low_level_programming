9d3a15d6e006a5713c7da21f27d72b2266d25d6b
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-15 15:49:12 -0700

    Task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,6 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
 
 	if (n <= 0)
 	{
@@ -15,14 +14,16 @@ void print_diagonal(int n)
 	}
 	else
 	{
+		int i, j;
+
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
-			if (j == i)
-				_putchar('\\');
-			else if (j < i)
-				_putchar(' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
 			_putchar('\n');
 		}
