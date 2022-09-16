4e6f93c23c6bb2d26e40512bf39bb25c32069445
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-15 17:01:18 -0700

    TAsk

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int x = 612852475143;
+	long int pf;
+
+	for (pf = 2; pf < x; pf++)
+	{
+		if (x % pf == 0)
+		{
+			x = x/pf;
+		}
+	}
+	printf("%ld\n", pf);
+	return (0);
+}
