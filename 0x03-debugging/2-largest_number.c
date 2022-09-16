32390bfd88176500d2a1336c7c6631bdbd632794
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-15 18:05:47 -0700

    TaskDebugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,20 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
 	else
 	{
 		largest = c;
 	}
+
+	return (largest);
+}
 	return (largest);
 }
