dc7fdd8875e4cc56a42324a5d46fd77a9b2c8540
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-20 15:26:07 -0700

    task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - prints n element of array followed by a new line
+ * @a: int 1 array to be printed
+ * @n: int 2 number of element to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i == 0)
+		{
+			printf("Array %d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
