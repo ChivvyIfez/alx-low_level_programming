5ceeb9cb6160cb6272bb7d95dfa54d25efc7cbd7
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-20 14:26:57 -0700

    Task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: parameter
+ */
+
+void print_rev(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	k = i;
+
+	j = k - 1;
+	while (j >= 0)
+	{
+		_putchar(s[j]);
+		j++;
+	}
+	_putchar(10);
+}
