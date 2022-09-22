4a1f6656952a0a341df1d00fba4099ec96741eb5
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-22 15:26:05 -0700

    Task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integer
+ * @a: array to be reversed
+ * @n: n element of the array
+ */
+
+void reverse_array(int *a, int *n)
+{
+	int i, j, k;
+
+	j = n - 1;
+
+	i = 0;
+	while (i < n / 2)
+	{
+		k = a[i];
+		a[i] = a[j];
+		a[j--] = k;
+		i++;
+	}
+}
