aba7e61ba40b7b99ef6615f62e2d8d1bd86070d0
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-22 17:48:24 -0700

    My commit on Pointer, Arrays and String

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: number 1
+ * @n2: number 2
+ * @r: buffer
+ * @size_r: buffer size
+ *
+ * Return: 0 or r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
