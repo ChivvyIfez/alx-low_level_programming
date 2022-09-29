8a2fab2d9479ab64720bb68affe3e44db49609f3
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-28 18:27:05 -0700

    Advance Task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns string palindrome
+ * @s: string
+ * Return: 1 if s is palindrome and 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (checker(s, 0, _strlen_recursion(s) - 1));
+}
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * checker - checks each character
+ * @s: string to be checked
+ * @a: checker
+ * @b: checker1
+ * Return: 0
+ */
+int checker(char *s, int a, int b)
+{
+	if (*(s + a) == *(s + b))
+	{
+		if (a == b || a == b + 1)
+			return (1);
+		return (0 + checker(s, a + 1, b - 1));
+	}
+	return (0);
+}
