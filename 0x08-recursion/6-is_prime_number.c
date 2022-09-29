f6baa90cf4f7819769d0fd8e0c5019bc2077647e
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-09-28 17:43:18 -0700

    Task on Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int the_prime_num(int x, int y);
+int prime_num(int x, int y);
 
 /**
  * is_prime_number - returns 1 if the input is prime number
@@ -12,29 +12,25 @@ int the_prime_num(int x, int y);
 
 int is_prime_number(int n)
 {
-	int i;
-
-	i = 2;
-	return (the_prime_num(i, n));
-}
+	if (n <= 1)
+		return (0);
+	return (prime_num(n, 2));
 
 /**
- * the_prime_num - prints prime numbers
+ * prime_num - prints prime numbers
  * @x: num
  * @y: num1
  *
  * Return: 1 if yes and 0 otherwise
  */
-int the_prime_num(int x, int y)
+int prime_num(unsigned int x, unsigned int y)
 {
-	if (x != 1)
+	if (x % y == 0)
 	{
 		if (x == y)
 			return (1);
-		else if (x % y == 0 || x < 1)
-			return (0);
 		else
-			return (the_prime_num(y + 1, x));
-	} else
-		return (0);
+			return (0);
+	}
+	return (0 + prime_num(x, y + 1));
 }
