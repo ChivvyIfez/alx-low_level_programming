588426b72271df525f4c8de29a8dbc60337c8d1a
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-10-05 01:17:14 -0700

    Arguments Task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,22 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-	int i, prod;
-
-	prod = 1;
-
-	if (argc > 3)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			prod = prod * atoi(argv[i]);
-			printf("%d\n", prod);
-		}
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
+	}
+
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
