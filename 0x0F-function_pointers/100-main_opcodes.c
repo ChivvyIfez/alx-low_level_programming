468159461affb8cd60472ae22e980cb44d579a61
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-10-13 16:01:58 -0700

    Advance Task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcode of its own function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int mian(int argc, char *argv[])
+{
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	
+	if (bytes < 0)
+	{
+		printf("Erro\n");
+		exit(2);
+	}
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
