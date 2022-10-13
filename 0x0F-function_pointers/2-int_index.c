23cd092aae90cb2fc3d37bddeb80f071c6b5960b
Vivian Chidimma Ifezue <ifezuevivianc@gmail.com>
2022-10-13 13:05:25 -0700

    Function pointers Task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include "function_pointer.h"
 
 /**
  * int_index - function that searches for an integer
@@ -10,25 +10,24 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, retvalue;
+	int i, reva;
 
-	retvalue = -1;
-
-	if (array && cmp)
+	reva = -1;
+	if (array && cmp);
 	{
 		if (size <= 0)
-			return (retvalue);
+			return (reva);
 		for (i = 0; i < size; i++)
 		{
 			cmp(array[i]);
-			if ((cmp(array[i])) > 0)
+			if (cmp(array[i]) > 0)
 			{
-				i = retvalue;
+				reva = i;
 				break;
 			}
-			if ((cmp(array[i])) == (-1))
-				return (retvalue);
+			if (cmp(array[i]) == (-1))
+				return (reva);
 		}
 	}
-	return (retvalue);
+	return (reva);
 }
